00e01ad70d9ec3e33cca3792ecbfd9b8945a023a
lucas <alenc09@protonmail.com>
2021-06-08 18:07:30 -0300

    commit para subir script com figuras e mapas para o tópico de forest-food.

--- 02.data_analysis.R
+++ 02.data_analysis.R
@@ -15,8 +15,8 @@ map_caat@data%>%
         "Bovine heard" = bovMed_18,
          "Goat heard" = capMed_18)%>%
 ggcorr(method = c("pairwise","spearman"), label = T,
-                   hjust = 0.7, vjust = 3,layout.exp = 3, label_round = 4)
-
+                   hjust = 0.8, vjust = 3,layout.exp = 3, label_round = 4)-> corr.food
+ggsave(plot = corr.food, filename = "corr.food.png")
 
 
 lm(data = map_caat@data, nvcPerc_18 ~ capMed_18 + bovMed_18 + drought_exposure_com)->mod.nvc_food
--- 03.figures.R
+++ 03.figures.R
@@ -5,17 +5,177 @@ source("02.data_analysis.R")
 
 #library####
 library(sf)
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+library(geobr)
+library(ggpubr)
+
+## data####
+st_as_sf(map_caat)->sf.map_caat
+uf<-read_state()
+uf %>% 
+  filter(abbrev_state %in% c("MG","BA","SE","PE", "AL","PB","RN","CE","PI"))->uf_caat
 
 #maps####
 ##nvc_2018####
-st_as_sf(map_caat)->sf.map_caat
+ggplot() +
+  geom_sf(data = sf.map_caat$geometry,
+          aes(fill = sf.map_caat$nvcPerc_18),
+          lwd = 0) +
+  scale_fill_distiller(
+    type = "seq",
+    palette = "Greens",
+    direction = 1,
+    name = "Native vegetation\ncover (%)",
+    na.value = "Grey70"
+  ) +
+  geom_sf(
+    data = uf_caat,
+    fill = "transparent",
+    color = "black",
+    lwd = 0.2
+  ) +
+  coord_sf(xlim = c(-48,-34), ylim = c(-17.1,-3)) +
+  geom_text(data = uf_caat, aes(
+    x = c(-42,-39.5, -36.5, -35.5,-34.5,-34.4,-36,-39, -42.4),
+    y = c(-16.8,-15,-11,-10,-8.5,-7,-4.7,-2.9,-5),
+    label = c("MG", "BA", "SE", "AL", "PE", "PB", "RN", "CE", "PI")
+  ),
+  size = 2.5) +
+  theme(
+    text = element_text(family = '', size = 6),
+    panel.grid.major = element_blank(),
+    panel.background = element_blank(),
+    axis.title = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.key.size = unit(5, "mm"),
+    legend.position = c(0.85, 0.2),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12)
+  ) -> nvc.caat_18
 
+##cattle 2018####
+ggplot() +
+  geom_sf(data = sf.map_caat$geometry,
+          aes(fill = sf.map_caat$bovMed_18),
+          lwd = 0) +
+  scale_fill_distiller(
+    type = "seq",
+    palette = "Reds",
+    direction = 1,
+    name = "Mean cattle\nherd",
+    na.value = "Grey70"
+  ) +
+  geom_sf(
+    data = uf_caat,
+    fill = "transparent",
+    color = "black",
+    lwd = 0.2
+  ) +
+  coord_sf(xlim = c(-48,-34), ylim = c(-17.1,-3)) +
+  geom_text(data = uf_caat, aes(
+    x = c(-42,-39.5, -36.5, -35.5,-34.5,-34.4,-36,-39, -42.4),
+    y = c(-16.8,-15,-11,-10,-8.5,-7,-4.7,-2.9,-5),
+    label = c("MG", "BA", "SE", "AL", "PE", "PB", "RN", "CE", "PI")
+  ),
+  size = 2.5) +
+  theme(
+    text = element_text(family = '', size = 6),
+    panel.grid.major = element_blank(),
+    panel.background = element_blank(),
+    axis.title = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.key.size = unit(5, "mm"),
+    legend.position = c(0.85, 0.2),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12)
+  ) -> bov.caat_18
 
-ggplot()+  
-  geom_sf(data=sf.map_caat$geometry, aes(fill = sf.map_caat$nvcPerc_18), lwd = 0)+
-  scale_fill_continuous()#+
-  #geom_sf(data=uf_caat, fill="transparent")+
-  #coord_sf(expand = FALSE)+
-  #labs(subtitle="Human capital", size=8, fill = "Vulnerability") +
-  #theme_map()+
-  #theme(legend.position = "none")
+##goat 2018####
+ggplot() +
+  geom_sf(data = sf.map_caat$geometry,
+          aes(fill = sf.map_caat$capMed_18),
+          lwd = 0) +
+  scale_fill_distiller(
+    type = "seq",
+    palette = "Reds",
+    direction = 1,
+    name = "Mean goat\nherd",
+  ) +
+  geom_sf(
+    data = uf_caat,
+    fill = "transparent",
+    color = "black",
+    lwd = 0.2
+  ) +
+  coord_sf(xlim = c(-48,-34), ylim = c(-17.1,-3)) +
+  geom_text(data = uf_caat, aes(
+    x = c(-42,-39.5, -36.5, -35.5,-34.5,-34.4,-36,-39, -42.4),
+    y = c(-16.8,-15,-11,-10,-8.5,-7,-4.7,-2.9,-5),
+    label = c("MG", "BA", "SE", "AL", "PE", "PB", "RN", "CE", "PI")
+  ),
+  size = 2.5) +
+  theme(
+    text = element_text(family = '', size = 6),
+    panel.grid.major = element_blank(),
+    panel.background = element_blank(),
+    axis.title = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.key.size = unit(5, "mm"),
+    legend.position = c(0.8, 0.2),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12, vjust = 1.5)
+  )-> cap.caat_18
+
+## Drought exposure ####
+ggplot() +
+  geom_sf(data = sf.map_caat$geometry,
+          aes(fill = sf.map_caat$drought_exposure_com),
+          lwd = 0) +
+  scale_fill_distiller(
+    type = "seq",
+    palette = "Blues",
+    direction = -1,
+    name = "Drought exposure",
+    na.value = "Grey70"
+  ) +
+  geom_sf(
+    data = uf_caat,
+    fill = "transparent",
+    color = "black",
+    lwd = 0.2
+  ) +
+  coord_sf(xlim = c(-48,-34), ylim = c(-17.1,-3)) +
+  geom_text(data = uf_caat, aes(
+    x = c(-42,-39.5, -36.5, -35.5,-34.5,-34.4,-36,-39, -42.4),
+    y = c(-16.8,-15,-11,-10,-8.5,-7,-4.7,-2.9,-5),
+    label = c("MG", "BA", "SE", "AL", "PE", "PB", "RN", "CE", "PI")
+  ),
+  size = 2.5) +
+  theme(
+    text = element_text(family = '', size = 6),
+    panel.grid.major = element_blank(),
+    panel.background = element_blank(),
+    axis.title = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.key.size = unit(5, "mm"),
+    legend.position = c(0.85, 0.2),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12, vjust = 1.5)
+  )-> seca.caat_18
+
+ggarrange(nvc.caat_18, seca.caat_18, bov.caat_18, cap.caat_18)->fig.food.map
+ggsave(plot = fig.food.map, filename = "fig.food.map.png")
